7943d7c482d5de33a85205d7ec524223dc5bcb05
johnpaulrusso <jprusso93@gmail.com>
2022-11-28 09:23:43 -0500

    Initial implementation of the logger component.
    Initial implementation of the logger component.

---
+++ src/lib/TextLogger.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import type { ILoggerModel } from "./LoggerModel";
+
+    export let model: ILoggerModel;
+</script>
+
+<div class="container">
+    <div class="datetime-col">
+        {#each model.messages as m}
+            <div>{m.datetime}</div>
+        {/each}
+    </div>
+    <div class="message-col">
+        {#each model.messages as m}
+            <div>{m.message}</div>
+        {/each}
+    </div>
+</div>
+
+<style>
+    .container{
+        overflow-y: auto;
+        display: flex;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
 	import { LoggerController } from "$lib/LoggerController";
+	import TextLogger from "$lib/TextLogger.svelte";
 
     let loggerController = new LoggerController();
 
     loggerController.LogString("Hello World!");
+    loggerController.LogString("This is a text logger demo.");
+    loggerController.LogString("Example Usage: loggerController.LogString(\"Hello World!\")");
 </script>
 
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<TextLogger model={loggerController.model}></TextLogger>
